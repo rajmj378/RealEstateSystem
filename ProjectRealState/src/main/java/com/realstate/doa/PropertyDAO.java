66479d641490fded820b1144703f65e02abe7ef5
unknown <surajbyanju47@gmail.com>
2014-11-18 11:46:18 -0600

    property dao

---
+++ ProjectRealState/src/main/java/com/realstate/doa/PropertyDAO.java
@@ -0,0 +1,38 @@
+package com.realstate.doa;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.realstate.property.Property;
+
+public class PropertyDAO extends ClientDAO{
+
+	public void getAllPerson(){
+		List<Property> properties = null;
+		ResultSet rs = this.exec("Select * from property");
+		  try {
+			while (rs.next()) {
+				  Property p = new Property();
+			       
+			        try {
+						p.setId(Integer.parseInt(rs.getString("id")));
+						p.setAddress(rs.getString("address"));
+						p.setNumberOfRooms(Integer.parseInt(rs.getString("numberOfRooms")));
+						p.setPrice(Double.parseDouble(rs.getString("price")));
+						properties.add(p);
+					} catch (NumberFormatException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			    }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
