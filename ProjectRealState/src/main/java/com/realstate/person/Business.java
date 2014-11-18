de399045a3348adff0a9f429af0474c8a7a9bcae
Ganesh Bhattarai <gannu_baba43@yahoo.com>
2014-11-18 14:44:18 -0600

    "ADDed attribute in business"

--- ProjectRealState/src/main/java/com/realstate/person/Business.java
+++ ProjectRealState/src/main/java/com/realstate/person/Business.java
@@ -1,5 +1,26 @@
 package com.realstate.person;
 
 public class Business extends Owner {
+	
+	private String businessName;
+	private String businessTye;
+	
+	public String getBusinessName() {
+		return businessName;
+	}
+
+	public String getBusinessTye() {
+		return businessTye;
+	}
+
+	public void setBusinessName(String businessName) {
+		this.businessName = businessName;
+	}
+
+	public void setBusinessTye(String businessTye) {
+		this.businessTye = businessTye;
+	}
+	
+	
 
 }
